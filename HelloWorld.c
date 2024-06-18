96ec23f2e271606c07be0ac817b344f548fe5855
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-06-18 11:55:01 +0300

    Hello World

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+/*
+* This is just an intro to more to come
+*I'm studying C Seriously this time for me to be
+*Good at programming IoT Devices using Arduino
+*which also includes Robotics
+*/
+
+#include <stdio.h>;
+
+int main(){
+    printf("Hello World! ");
+    printf("Seriously This Time");
+
+    return 0;
+}
