838c6de161195fff1d7b223f9b55fd6b4d39a7c1
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-06-19 12:01:40 +0300

    OutPUt and Escape Sequences

---
+++ OutPut.c
@@ -0,0 +1,15 @@
+/*This program is all about the Output
+*I learned about some new ESCAPE SEQUENCE
+*such as:-
+*\t- Creates a Horizontal tab
+*\\- Inserts a backlash character(\)
+\"-  Inserts a double quote character
+\n- Next Line
+*/
+#include <stdio.h>;
+
+int main(){
+    printf("Ochieng \t Bryian \t Owuor \n");
+    printf("OCBROW\\ BRAWOR \" Can DO");
+    return 0;
+}
